6afee26f2b82b2c910d405ba6c020becd229f03e
최미래 <miraechoi@dgsw.hs.kr>
2025-05-30 18:34:07 +0900

    qoom sync at Fri May 30 2025 02:34:07 GMT-0700 (Pacific Daylight Time)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# qoom-github-publish-task
-This is your first repo!
+Animated Web Card for Father's Day
+=====
+
+This project was built by Qoom.   
+Check a <a href="https://app.qoom.io/tutorials/webcard/guide.md" target="_blank">tutorial</a> to see how to build this.
---
+++ script.js
@@ -0,0 +1,10 @@
+function openUp() {
+	// Puts our <section id='cover'> element in a variable
+	const $opencover = document.querySelector('#cover');
+	
+	// Adds a `class` attribute with value `animate` to our cover element
+	$opencover.classList.add('animate');
+	
+	//OUTPUT:
+	// <section id='cover' class='animate'>
+}
